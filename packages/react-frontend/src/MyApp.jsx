ac71d0265b06c389ea79a29c817d84ed67d6eed1
BrKap <259brian952@gmail.com>
2025-01-09 20:32:01 -0800

    Table removeOneCharacter

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,32 +1,30 @@
 // src/MyApp.jsx
-import React from "react";
+
+import React, { useState } from "react";
 import Table from "./Table";
 
-const characters = [
+function MyApp() {
+  const [characters, setCharacters] = useState([
     {
       name: "Charlie",
-      job: "Janitor"
-    },
-    {
-      name: "Mac",
-      job: "Bouncer"
-    },
-    {
-      name: "Dee",
-      job: "Aspring actress"
-    },
-    {
-      name: "Dennis",
-      job: "Bartender"
+      job: "Janitor" // the rest of the data
     }
-  ];
+  ]);
 
-function MyApp() {
+  function removeOneCharacter(index) {
+    const updated = characters.filter((character, i) => {
+      return i !== index;
+    });
+    setCharacters(updated);
+  }
+
   return (
     <div className="container">
-      <Table characterData={characters} />
+      <Table
+        characterData={characters}
+        removeCharacter={removeOneCharacter}
+      />
     </div>
   );
 }
 
-export default MyApp;
