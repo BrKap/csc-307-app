a2b32a34bfde11de80c77f9065a78f01d9697107
BrKap <259brian952@gmail.com>
2025-01-14 22:35:28 -0800

    Step 6 Completed

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -76,5 +76,16 @@ app.get("/users/:id", (req, res) => {
   }
 });
 
+const addUser = (user) => {
+  users["users_list"].push(user);
+  return user;
+};
+
+app.post("/users", (req, res) => {
+  const userToAdd = req.body;
+  addUser(userToAdd);
+  res.send();
+});
+
 
 
