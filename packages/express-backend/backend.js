0b2c73dfe6bdde9a17ca6f99a9a982a02d95c89b
BrKap <259brian952@gmail.com>
2026-04-30 17:09:47 -0700

    Added mongodb. Changed all endpoints to run through mongoose and separated by different file concepts with user.js and user-services.js. Fixed issue of mongoose trying to connect to IPv6 instead of IPv4 (why it did that idk). Changed frontend id to mongoose _id.

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,6 +1,7 @@
 // backend.js
 import express from "express";
 import cors from "cors";
+import userServices from "./user-services.js";
 
 const app = express();
 const port = 8000;
@@ -18,117 +19,47 @@ app.listen(port, () => {
   );
 });
 
-const users = {
-  users_list: [
-    {
-      id: "xyz789",
-      name: "Charlie",
-      job: "Janitor"
-    },
-    {
-      id: "abc123",
-      name: "Mac",
-      job: "Bouncer"
-    },
-    {
-      id: "ppp222",
-      name: "Mac",
-      job: "Professor"
-    },
-    {
-      id: "yat999",
-      name: "Dee",
-      job: "Aspring actress"
-    },
-    {
-      id: "zap555",
-      name: "Dennis",
-      job: "Bartender"
-    }
-  ]
-};
-
-const findUserByName = (name) => {
-  return users["users_list"].filter(
-    (user) => user["name"] === name
-  );
-};
-
-const findUserByJob = (job) => {
-  return users["users_list"].filter(
-    (user) => user["job"] === job   
-  );
-}
-
-const findUserByNameAndJob = (name, job) => {
-  return users["users_list"].filter(
-    (user) => user["name"] === name && user["job"] === job
-  );
-}
-
 app.get("/users", (req, res) => {
   const name = req.query.name;
   const job = req.query.job;
-  if (name != undefined && job != undefined) {
-    let result = findUserByNameAndJob(name, job);
-    result = { users_list: result };
-    res.send(result);
-  } else if (name != undefined) {
-    let result = findUserByName(name);
-    result = { users_list: result };
-    res.send(result);
-  } else if (job != undefined) {
-    let result = findUserByJob(job);
-    result = { users_list: result };
-    res.send(result);
+
+  if (name && job) {
+    userServices.findUserByNameAndJob(name, job)
+      .then((result) => res.send({ users_list: result }))
+      .catch(() => res.status(500).send("Server error."));
   } else {
-    res.send(users);
+    userServices.getUsers(name, job)
+      .then((result) => res.send({ users_list: result }))
+      .catch(() => res.status(500).send("Server error."));
   }
 });
-
-const findUserById = (id) =>
-  users["users_list"].find((user) => user["id"] === id);
 
 app.get("/users/:id", (req, res) => {
-  const id = req.params["id"]; //or req.params.id
-  let result = findUserById(id);
-  if (result === undefined) {
-    res.status(404).send("Resource not found.");
-  } else {
-    res.send(result);
-  }
+  userServices.findUserById(req.params.id)
+    .then((result) => {
+      if (result === null) {
+        res.status(404).send("Resource not found.");
+      } else {
+        res.send(result);
+      }
+    })
+    .catch(() => res.status(500).send("Server error."));
 });
-
-const addUser = (user) => {
-  users["users_list"].push(user);
-  return user;
-};
-
-const generateId = () => {
-  return Math.random().toString(36).substring(2, 9);
-};
 
 app.post("/users", (req, res) => {
-  const userToAdd = req.body;
-  userToAdd.id = generateId();
-  const newUser = addUser(userToAdd);
-  res.status(201).send(newUser);
+  userServices.addUser(req.body)
+    .then((result) => res.status(201).send(result))
+    .catch(() => res.status(500).send("Server error."));
 });
 
-const deleteUserById = (id) => {
-  const index = users["users_list"].findIndex((user) => user["id"] === id);
-  if (index !== -1) {
-    return users["users_list"].splice(index, 1); 
-  }
-  return null;
-};
-
 app.delete("/users/:id", (req, res) => {
-  const id = req.params["id"]; //or req.params.id
-  let result = deleteUserById(id);
-  if (!result) {
-    res.status(404).send("Resource not found.");
-  } else {
-    res.status(204).send();
-  }
+  userServices.deleteUserById(req.params.id)
+    .then((result) => {
+      if (result === null) {
+        res.status(404).send("Resource not found.");
+      } else {
+        res.status(204).send();
+      }
+    })
+    .catch(() => res.status(500).send("Server error."));
 });
